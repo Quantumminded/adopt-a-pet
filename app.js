4912ae1d380a496723510819c1e35f8e8dd74b9b
Luca <lukkino31@gmail.com>
2022-11-18 12:34:22 +0100

    name of pet displayed

--- app.js
+++ app.js
@@ -17,6 +17,21 @@ app.get('/', (req, res) => {
     res.send(`<h1>List of pets</h1>`)
 })
 
+app.get('/animals/:pet_type', (req, res) => {
+    let pet_type = req.params.pet_type;
+    let  html='';
+
+    Object.keys(pets).forEach(function (key) {
+        if (key === pet_type) {
+          for(i=0;i<pets[key].length;i++)
+            html += `<li> ${pets[key][i].name} </li>`
+        }
+    })
+    let results = `<ul> ${html} </ul>`
+
+    res.send(`<h1>List of ${pet_type} </h1> ${results} `)
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
