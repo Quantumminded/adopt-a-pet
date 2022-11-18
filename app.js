bada7f42130be12298497ddbf2b2c36febf096d7
Luca <lukkino31@gmail.com>
2022-11-18 12:35:30 +0100

    fixing typo

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const pets = require('./helper')
+
 app.get('/', (req, res) => {
     res.send (`
       <h1>Adopt a Pet!</h1>
